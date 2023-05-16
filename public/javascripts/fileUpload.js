61b789b5cbbd638887a3954ae0645ac20cfc9b6b
EveKoss <ekoss.prog@gmail.com>
2023-05-16 15:24:33 +0400

    Deleted unnecessary files

--- public/javascripts/fileUpload.js
+++
@@ -1,8 +0,0 @@
-{/* <script>
-    FilePond.registerPlugin(FilePondPluginImagePreview);
-    FilePond.registerPlugin(FilePondPluginImageResize);
-    FilePond.registerPlugin(FilePondPluginFileEncode);
-    const inputElement = document.querySelector('input[type="file"]');
-    const pond = FilePond.create(inputElement);
-    FilePond.parse(document.body);
-    </script> */}
